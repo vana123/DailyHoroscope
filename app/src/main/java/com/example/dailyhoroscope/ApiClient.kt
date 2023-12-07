95e93c6f9cf02323d3739e4845566b6706f9dc2e
vana123 <savych.vana@gmail.com>
2023-12-07 14:46:18 +0200

    Add object ApiClient

---
+++ app/src/main/java/com/example/dailyhoroscope/ApiClient.kt
@@ -0,0 +1,27 @@
+package com.example.dailyhoroscope
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+
+    private const val BASE_URL = "http://sandipbgt.com/theastrologer/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .hostnameVerifier { _, _ -> true }
+        .build()
+
+    val astrologyApi: AstrologyApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AstrologyApi::class.java)
+    }
+}
